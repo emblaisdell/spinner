6dc1bd8903fb9f42b9160fb4ba08c077c8169d34
Eben Blaisdell <emblaisdell@foresthills.net>
2023-10-24 12:34:25 -0400

    tweak numbers

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from solid2 import *
 
 phi = (1 + sqrt(5)) * 0.5
 dR = 2 - phi
-N = 100  # num time steps
-dt = 1 / N  # time step length
+N = 150  # num time steps
+dt = 0.01  # time step length
 
 BLOCK = lambda: cube(0.15, 0.25, 0.75, center=True)
 AXIS_OF_ROT = [0, 1, 0]
-omega = 180  # angular velocity
+omega = 120  # angular velocity
 
 
 def posn(t):
@@ -29,7 +29,9 @@ def block_at_n(n):
 
 def main():
     blocks = [block_at_n(n) for n in range(N)]
-    piece = union()(blocks).rotateX(-90)
+    piece = union()(blocks) + sphere(r=0.9, _fn=100)
+    piece -= cube(4, center=True).translate(0, 0, -2)
+    piece = piece.rotateX(-90)
     piece.save_as_scad()
 
 
